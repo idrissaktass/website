1776148039a8f11569e2a78daacfd6559a13015e
idrissaktass <idrissaktass98@gmail.com>
2024-12-28 16:26:54 +0300

    upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -74,7 +74,7 @@ const Home = () => {
                         sx={{ fontWeight: '500', color: 'white' }}
                         >
                         Hi, I'm{' '}
-                        <span style={{ color: '#BFD49E' }}>İdris Aktaş</span>
+                        <span style={{ color: '#BFD49E' }}>Elif Toraman</span>
                         </Typography>
                         <Typography fontSize={{xs:"32px", md:"24px", lg:"32px"}} fontFamily={"Hagrid"} color="white">
                             and I'm a Frontend Developer.
@@ -88,7 +88,7 @@ const Home = () => {
                     >
                     <Typography fontSize={{xs:"24px", md:"20px", lg:"24px"}}  color="white">Contact me</Typography>
                     </Button>
-                    <Typography  fontSize={{xs:"20px", md:"20px", lg:"24px"}} color='white'>via idrissaktass98@gmail.com</Typography>
+                    <Typography  fontSize={{xs:"20px", md:"20px", lg:"24px"}} color='white'>via eliftoraman99@gmail.com</Typography>
                 </Grid>
                 </Grid>
                 <Grid width={{xs:"100%", md:"fit-content"}} fontFamily={"Hagrid"} display={{xs:"flex", md:"unset"}} justifyContent={{xs:"end", md:"unset"}}>
@@ -113,7 +113,7 @@ const Home = () => {
          }}>
                 <Grid size={{xs:12}}>
                 <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E">
-                About İdris Aktaş
+                About Elif Toraman
             </Typography>
             <Typography fontSize={"20px"} fontFamily={"Hagrid"} color='white'>
                 Hi there! 👋 I'm a passionate Web Developer with a degree in Computer Engineering from Cukurova University. I specialize in React, TypeScript, and the MERN Stack, and I’m constantly improving my skills to stay ahead in the ever-evolving world of web development.
@@ -122,8 +122,8 @@ const Home = () => {
                 Thank you for visiting my website! Feel free to explore my projects and connect with me for collaboration.   
                 </Typography>
                 </Grid>
-            <Grid mt={3} size={{xs:12}}
-                height={{xs:"400px", sm:"500px", md:"60%"}} 
+            {/* <Grid mt={3} size={{xs:12}}
+                height={{xs:"400px", sm:"500px", md:"600px"}} 
                 overflow="hidden" 
                 sx={{ objectFit:"cover",
                     display: 'flex', 
@@ -139,220 +139,93 @@ const Home = () => {
                     height: 'auto',
                     }} 
                 />
-                </Grid>
+                </Grid> */}
         </Grid>
         <Grid container justifyContent={"center"} alignItems={"center"} textAlign={"start"} gap={3}
             ref={(el) => sectionRefs.current[2] = el}    
             sx={{ width:{xs:"95%", sm:"90%", md:"85%", lg:"80%"},
-                height: {xs:"85vh", md:"90vh"},
                 ...sectionStyle(sectionDirections[2]),
                 ...(visibleSections[2] ? visibleStyle : {}),
             }} 
         >
-            <Grid height={"fit-content"}>
-                <Typography fontSize={{xs:"36px", md:"42px", lg:"48px"}}  fontFamily={"Hagrid"} color="white">
-                    Credentials
-                </Typography>
-            </Grid>
-            <Grid container mb={6} justifyContent={'space-between'} alignItems={'start'} >
-                <Grid size={{xs:12, md:4}} mt={3}
-                    height="60%" 
-                    overflow="hidden" 
-                    sx={{
-                        display: {xs:"none", md:'flex'}, 
-                        alignItems: 'center', 
-                        justifyContent: 'center',
-                    }}
-                    >
-                    <Box
-                        component="img" 
-                        src="/eagle.png" 
-                        sx={{
-                        width: '100%', 
-                        height: 'auto',
-                        filter: "brightness(1.2) contrast(0.7) saturate(1.5)"
-                    }} 
-                    />
+                <Grid size={{xs:12}} display={'flex'} flexDirection={"column"} justifyContent={"center"} alignItems={'center'}>
+                    <Grid size={{xs:12}} display={'flex'} gap={2} alignItems={'center'}>
+                        <Grid size={{xs:12, md:8}} mt={3}
+                            height="300px" 
+                            width={"100%"}
+                            overflow="hidden" 
+                            display={"flex"}
+                            sx={{
+                                alignItems: 'center', 
+                                justifyContent: 'center',
+                            }}
+                            >
+                            <Box
+                                component="img" 
+                                src="/graduation.jpg" 
+                                sx={{  objectFit:"cover",
+                                width: '100%', 
+                                height: '450px',
+                                filter: "brightness(1.2) contrast(0.7) saturate(1.5)"
+                            }} 
+                            />
+                        </Grid>
+                        <Grid size={{xs:12, md:4}} display={'flex'} justifyContent={'space-between'} flexDirection={"column"} alignItems={'center'}>
+                            <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E" textAlign={"start"}>
+                                Education
+                                <Typography  fontSize={"20px"} fontFamily={"Hagrid"} color="white" width={"100%"}>
+                                <b>BS Computer Engineering</b> <br/>
+                                Cukurova University
+                                </Typography>
+                                <Typography  fontSize={"20px"} fontFamily={"Hagrid"} color="white" width={"100%"}>
+                                <b> M.Sc. Computer Science</b> <br/>
+                                University of Klagenfurt
+                                </Typography>
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid size={{xs:12, md:6}} display={'flex'} flexDirection={"column"} gap={8}>
-                    <Box display={'flex'} justifyContent={'space-between'}>
-                        <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E" textAlign={"start"}>
-                            Education
-                        </Typography>
-                        <Typography fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontFamily={"Hagrid"} color="white" textAlign={"end"} width={"100%"}>
-                            BS Computer Engineering <br/>
-                            Cukurova University
-                        </Typography>
-                    </Box>
-                    <Box display={'flex'} justifyContent={'space-between'}>
-                        <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E">
-                            Experience
-                        </Typography>
-                        <Typography fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontFamily={"Hagrid"} color="white" textAlign={"end"} width={"100%"}>
-                            Frontend Developer <br/>
-                            PokaMind
-                        </Typography>
-                    </Box>
-                    <Box display={'flex'} justifyContent={'space-between'}>
-                        <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E">
-                            Skills
-                        </Typography>
-                        <Typography fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontFamily={"Hagrid"} color="white" textAlign={"end"} width={"100%"}>
-                            Javascript, HTML, CSS <br/>
-                            React <br/>
-                            MERN Stack <br/> 
-                            Material UI
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid size={{xs:12, md:4}} mt={3}
-                    overflow="hidden" 
-                    sx={{
-                        display: {xs:"flex", md:'none'}, 
-                        alignItems: 'center', 
-                        justifyContent: 'center',
-                        position:"relative",
-                        top:"-350px",
-                    }}
-                    >
-                    <Box 
-                        component="img" 
-                        src="/eagle.png" 
-                        sx={{
-                        width: '100%', 
-                        height: 'auto',
-                        filter: "brightness(1.2) contrast(0.6) saturate(1.5)",
-                        opacity:"0.7"
-                    }} 
-                    />
-                </Grid>
-            </Grid>
-
         </Grid>
-        <Grid container gap={3} textAlign={"start"}
+        <Grid mb={5} container justifyContent={"center"} alignItems={"center"} textAlign={"start"} gap={3}
             ref={(el) => sectionRefs.current[3] = el}    
             sx={{ width:{xs:"95%", sm:"90%", md:"85%", lg:"80%"},
-                minHeight: "120vh",
                 ...sectionStyle(sectionDirections[3]),
                 ...(visibleSections[3] ? visibleStyle : {}),
             }} 
-            >
-            <Grid size={{xs:12}}>
-                <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E">
-                    My Practices
-                </Typography>
-            </Grid>
-            <Grid container justifyContent={"space-between"} mb={6}>
-            <Grid 
-                item 
-                size={{xs:12, md:5.5}}
-                mt={{ xs: "20px", md: "0px" }} 
-                onClick={() => window.location.href = 'https://www.lostandfoundtr.online/'}
-                sx={{
-                    cursor: "pointer", // Indicates clickable area
-                    "&:hover": {
-                    transform: "scale(1.01)", // Optional: Add hover effect for better UI
-                    transition: "transform 0.2s ease",
-                    },
-                }}
-                >
-                    <Typography fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontFamily={"Hagrid"} color="white">
-                        Lost and Found App
-                    </Typography>
-                    <Grid 
-                        mt={3}
-                        overflow="hidden"
-                        sx={{ objectFit:"cover",
-                            display: 'flex',
-                            alignItems: 'end',
-                            justifyContent: 'center',
-                            position: 'relative',
-                            height:"450px"
-                        }}
-                        >
-                        <Box 
-                            component="img" 
-                            src="/lost.png" 
-                            sx={{ objectFit:"cover",
-                            width: '100%', 
-                            height: '450px',
-                            }} 
-                        />
-                        <Typography paddingBottom={{xs:"5px", sm:"15px", md:"5px", lg:"15px"}} fontFamily={"Hagrid"}  fontSize={{xs:"20px", md:"20px", lg:"24px"}}
-                            textAlign={"center"}
-                            sx={{
-                            width:"100%",
-                            background: 'linear-gradient(to top, rgb(191, 212, 158), rgb(35 35 35 / 0%))',
-                            position: 'absolute',
-                            bottom: 0,
-                            left: '50%',
-                            transform: 'translateX(-50%)',
-                            color: 'white',
-                            }}
-                        >
-                            Developed a full-stack lost and found web application to help users post, search, and find lost items or pets. 
-                        </Typography>
+        >
+                                <Grid size={{xs:12}} display={"flex"} alignItems={'center'}>
+                        <Grid size={{xs:12, md:4}} display={'flex'} gap={1} flexDirection={"column"} alignItems={'center'}>
+                            <Typography fontSize={{xs:"24px", md:"28px", lg:"32px"}} fontFamily={"Hagrid"} color="#BFD49E">
+                                Skills
+                                <Typography  fontSize={"20px"} fontFamily={"Hagrid"} color="white" width={"100%"}>
+                                React <br/>
+                                Python <br/> 
+                                MERN Stack <br/> 
+                                Material UI <br/> 
+                                Bootstrap 
+                            </Typography>
+                            </Typography>
                         </Grid>
-
-                </Grid>
-                <Grid 
-                    item 
-                    size={{xs:12, md:5.5}}
-                    mt={{ xs: "20px", md: "0px" }} 
-                    onClick={() => window.location.href = 'https://www.cinescope.online'}
-                    sx={{
-                        cursor: "pointer", // Indicates clickable area
-                        "&:hover": {
-                        transform: "scale(1.01)", // Optional: Add hover effect for better UI
-                        transition: "transform 0.2s ease",
-                        },
-                    }}
-                    >
-                    <Typography fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontFamily={"Hagrid"} color="white">
-                        Movie App
-                    </Typography>
-                    <Grid 
-                        mt={3}
-                        overflow="hidden"
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'end',
-                            justifyContent: 'center',
-                            position: 'relative',
-                            height:"450px"
-                        }}
-                        >
-                        <Box 
-                            component="img" 
-                            src="/movie.png" 
-                            sx={{ objectFit:"cover",
-                            width: '100%', 
-                            height: '450px',
-                            }} 
-                        />
-                        <Typography paddingBottom={{xs:"5px", sm:"15px", md:"5px", lg:"15px"}} fontFamily={"Hagrid"} fontSize={{xs:"20px", md:"20px", lg:"24px"}}
-                            textAlign={"center"}
-                            sx={{
-                            width:"100%",
-                            background: 'linear-gradient(to top, rgb(191, 212, 158), rgb(35 35 35 / 0%))',
-                            position: 'absolute',
-                            bottom: 0,
-                            left: '50%',
-                            transform: 'translateX(-50%)',
-                            color: 'white',
-                            paddingInline:"7"
+                        <Grid size={{xs:12, md:8}}  mt={3}
+                            height="300px" 
+                            overflow="hidden" 
+                            display={"flex"}
+                            sx={{ 
+                                alignItems: 'start', 
+                                justifyContent: 'end',
                             }}
-                        >
-                            A movie application that allows users to find movies, enhancing their experience with personalized suggestions.
-                        </Typography>
+                            >
+                            <Box
+                                component="img" 
+                                src="/coding.jpg" 
+                                sx={{ objectFit:"cover",
+                                width: '100%', 
+                                height: '450px',
+                                filter: "brightness(1.2) contrast(0.7) saturate(1.5)"
+                            }} 
+                            />
                         </Grid>
-                </Grid>
-            </Grid>
-            <Grid container size={{xs:12}} justifyContent={'center'} mb={2}>
-                <Typography  textAlign={'center'} color='white' fontFamily={"Hagrid"} fontSize={{xs:"20px", md:"20px", lg:"24px"}} fontStyle={"italic"}>
-                    Growth is the journey itself; I am not perfect, but I am always striving for better.
-                </Typography>
-            </Grid>
+                    </Grid>
         </Grid>
     </Box>
   );
