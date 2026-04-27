d5f833ca7718ee46a505a9b844ae08cf3cd3aff7
idrissaktass <80005089+idrissaktass@users.noreply.github.com>
2026-04-27 11:42:06 +0300

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,274 +6,296 @@ import LaptopOutlinedIcon from "@mui/icons-material/LaptopOutlined";
 import { motion, useInView, useAnimation } from "framer-motion";
 
 const servicesData = [
-    {
-    title: "ChefAI",
-    description:
-      "Developed a full-stack AI-powered cooking assistant that suggests recipes, meal plans, and smart ingredient-based recommendations using modern web technologies.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://play.google.com/store/apps/details?id=com.idrisaktas.chefai&pcampaignid=web_share",
-  },
-    {
-    title: "Moodflix AI",
-    description:
-      "Built an AI-powered movie discovery app that provides personalized recommendations, real-time movie data, and social features.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://play.google.com/store/apps/details?id=com.idrisaktas.aimovie&pcampaignid=web_share",
-  },
-  {
-    title: "Lost and Found",
-    description:
-      "Developed a full-stack lost and found web application to help users post, search, and find lost items or pets.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://www.lostandfound.website/",
-  },
-    {
-    title: "Dashboard",
-    description:
-      "Developed a professional inventory dashboard featuring real-time stock tracking, automated alerts, and advanced data visualizations for optimized supply chain management.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://smart-dashboard-opal.vercel.app/",
-  },
-  {
-    title: "Diary AI",
-    description:
-      "A personalized AI-powered app that analyzes users' daily experiences and emotional states, providing detailed reports and weekly insights to help improve well-being.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://ai-diary-zeta.vercel.app/",
-  },
-  {
-    title: "Movie App",
-    description:
-      "A movie application that allows users to find movies, enhancing their experience with personalized suggestions.",
-    icon: <LaptopOutlinedIcon />,
-    link: "https://www.cinescope.live/",
-  },
-    {
-    title: "Weather App",
-    description:
-      "A simple and user-friendly weather app that provides real-time and 7-day forecasts with detailed city-based information.",
-    icon: <LaptopOutlinedIcon />,
-    link: "http://weather-pi-sand-22.vercel.app/",
-  },
+{
+title: "ChefAI",
+description:
+"Developed a full-stack AI-powered cooking assistant that suggests recipes, meal plans, and smart ingredient-based recommendations using modern web technologies.",
+icon: <LaptopOutlinedIcon />,
+link: "https://play.google.com/store/apps/details?id=com.idrisaktas.chefai&pcampaignid=web_share",
+},
+{
+title: "Moodflix AI",
+description:
+"Built an AI-powered movie discovery app that provides personalized recommendations, real-time movie data, and social features.",
+icon: <LaptopOutlinedIcon />,
+link: "https://play.google.com/store/apps/details?id=com.idrisaktas.aimovie&pcampaignid=web_share",
+},
+{
+title: "Lost and Found",
+description:
+"Developed a full-stack lost and found web application to help users post, search, and find lost items or pets.",
+icon: <LaptopOutlinedIcon />,
+link: "https://www.lostandfound.website/",
+},
+{
+title: "Dashboard",
+description:
+"Developed a professional inventory dashboard featuring real-time stock tracking, automated alerts, and advanced data visualizations.",
+icon: <LaptopOutlinedIcon />,
+link: "https://smart-dashboard-opal.vercel.app/",
+},
+{
+title: "Diary AI",
+description:
+"A personalized AI-powered app that analyzes users' daily experiences and emotional states.",
+icon: <LaptopOutlinedIcon />,
+link: "https://ai-diary-zeta.vercel.app/",
+},
+{
+title: "Movie App",
+description:
+"A movie application that allows users to find movies with personalized suggestions.",
+icon: <LaptopOutlinedIcon />,
+link: "https://www.cinescope.live/",
+},
+{
+title: "Weather App",
+description:
+"A simple weather app that provides real-time and 7-day forecasts.",
+icon: <LaptopOutlinedIcon />,
+link: "http://weather-pi-sand-22.vercel.app/",
+},
 ];
 
-const ServiceCard = ({ title, description, icon, link, index }) => {
-  const cardRef = useRef(null);
-  const [hovered, setHovered] = useState(false);
-  const isInView = useInView(cardRef, { once: true });
-  const controls = useAnimation();
-  const theme = useTheme();
-  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
-  const [opened, setOpened] = useState(false);
-  
-  const handleCardClick = () => {
-    window.open(link, "_blank");
-    console.log("toggleif")
-  };
+const ServiceCard = ({ title, description, icon, link, index, activeIndex }) => {
 
-  // const handleCardClick = (e) => {
-  //   if (!isMdUp) {
-  //     setOpened(!opened);
-  //     console.log("cardclick")
-  //   } else {
-  //     toggleCard();
-  //   }
-  // };
+const cardRef = useRef(null);
+const [hovered, setHovered] = useState(false);
+const isInView = useInView(cardRef, { once: true });
+const controls = useAnimation();
+const theme = useTheme();
+const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
 
-  useEffect(() => {
-    const startAnimation = async () => {
-      await controls.start({
-        scale: [1, 1.05, 1],
-        transition: { duration: 0.9, delay: index * 0.5 },
-      });
-    };
+const handleCardClick = () => {
+window.open(link, "_blank");
+};
 
-    const intervalId = setInterval(() => {
-      if (!hovered) {
-        startAnimation();
-      }
-    }, 4000);
+useEffect(() => {
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [controls, hovered, index]);
+if (activeIndex === index && !hovered) {
 
-  return (
-    <Grid item size={{ xs: 12, sm: 8, md: 5, lg: 2.6, xl: 2.6 }} ref={cardRef}
-    sx={{
-      cursor: isMdUp ? 'pointer' : 'default',
-    }}
-    onClick={isMdUp ? () => window.open(link, "_blank") : null}
-    >
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={isInView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 1, delay: index * 0.3 }}
-      >
-        <motion.div animate={controls}>
-          <Card
-            sx={{
-              backgroundColor: hovered ? "#0b260b" : "transparent",
-              p: 2,
-              border: "2px solid rgb(0, 0, 0)",
-              borderRadius: 3,
-              boxShadow: 3,
-              transition: "transform 0.3s, background-color 0.6s",
-              position: "relative",
-              minHeight: 200,
-              height: hovered ? "max-content" : 200,
-              overflow: "hidden",
-              "&:hover": { transform: "scale(1.02)" },
-            }}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-          >
-            <Box
-              sx={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                backgroundImage: "url('/practices.jpg')",
-                backgroundSize: hovered ? "180%" : "cover",
-                backgroundPosition: "center",
-                opacity: hovered ? 0 : 0.6,
-                transition:
-                  "background-size 0.6s ease, opacity 0.6s ease, transform 0.6s ease",
-                transform: hovered ? "scale(1.1)" : "scale(1)",
-                zIndex: 1,
-              }}
-            />
-            <CardContent onClick={isMdUp ? () => window.open(link, "_blank") : null}
-              sx={{
-                height: "10px",
-                mt: hovered ? 2 : 0,
-                position: "relative",
-                top: hovered ? "10%" : "45%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                textAlign: "center",
-                color: "white",
-                zIndex: 2,
-                transition: "top 0.3s",
-              }}
-            >
-              <Typography
-                variant="H32px"
-                color="textThird"
-                align="center"
-                bgcolor={hovered ? "none" : "#305041b8"}
-                border={hovered ? "none" : "2px solid #305041"}
-                borderRadius={hovered ? "none" : "10px"}
-                padding={hovered ? "0" : "4px"}
-              >
-                {title}
-              </Typography>
-            </CardContent>
+controls.start({
+scale: [1, 1.07, 1],
+transition: { duration: 0.8 }
+});
 
-            <motion.div
-              style={{
-                display: hovered ? "flex" : "none",
-                zIndex: 3,
-                gap: 10,
-                flexDirection: "column",
-                textAlign: "center",
-                alignItems: "center",
-                height: "100%",
-              }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: hovered ? 1 : 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Typography
-                variant="body1"
-                align="center"
-                color={hovered ? "white" : "textSecondary"}
-              >
-                {description}
-              </Typography>
-              <Box
-                borderRadius={"100%"}
-                bgcolor={"#61a05f"}
-                padding={"8px 8px 6px 10px"}
-                color={"#0b260b"}
-                onClick={handleCardClick}
-                sx={{
-                  cursor: "pointer",
-                  zIndex: 4,
-                }}
-              >
-                <ArrowForwardIosIcon />
-              </Box>
-            </motion.div>
-          </Card>
-        </motion.div>
-      </motion.div>
-    </Grid>
-  );
-}; 
+}
+
+}, [activeIndex]);
+
+return (
+<Grid
+item
+size={{ xs: 12, sm: 8, md: 5, lg: 2.6, xl: 2.6 }}
+ref={cardRef}
+sx={{
+cursor: isMdUp ? "pointer" : "default",
+}}
+onClick={isMdUp ? handleCardClick : null}
+>
+
+<motion.div
+initial={{ opacity: 0, y: 50 }}
+animate={isInView ? { opacity: 1, y: 0 } : {}}
+transition={{ duration: 1, delay: index * 0.2 }}
+>
+
+<motion.div animate={controls}>
+
+<Card
+sx={{
+backgroundColor: hovered ? "#0b260b" : "transparent",
+p: 2,
+border: "2px solid rgb(0, 0, 0)",
+borderRadius: 3,
+boxShadow: 3,
+transition: "transform 0.3s, background-color 0.6s",
+position: "relative",
+minHeight: 200,
+height: hovered ? "max-content" : 200,
+overflow: "hidden",
+"&:hover": { transform: "scale(1.02)" },
+}}
+onMouseEnter={() => setHovered(true)}
+onMouseLeave={() => setHovered(false)}
+>
+
+<Box
+sx={{
+position: "absolute",
+top: 0,
+left: 0,
+width: "100%",
+height: "100%",
+backgroundImage: "url('/practices.jpg')",
+backgroundSize: hovered ? "180%" : "cover",
+backgroundPosition: "center",
+opacity: hovered ? 0 : 0.6,
+transition: "background-size 0.6s ease, opacity 0.6s ease",
+zIndex: 1,
+}}
+/>
+
+<CardContent
+sx={{
+height: "10px",
+mt: hovered ? 2 : 0,
+position: "relative",
+top: hovered ? "10%" : "45%",
+left: "50%",
+transform: "translate(-50%, -50%)",
+textAlign: "center",
+color: "white",
+zIndex: 2,
+transition: "top 0.3s",
+}}
+>
+
+<Typography
+variant="H32px"
+color="textThird"
+align="center"
+bgcolor={hovered ? "none" : "#305041b8"}
+border={hovered ? "none" : "2px solid #305041"}
+borderRadius={hovered ? "none" : "10px"}
+padding={hovered ? "0" : "4px"}
+>
+{title}
+</Typography>
+
+</CardContent>
+
+<motion.div
+style={{
+display: hovered ? "flex" : "none",
+zIndex: 3,
+gap: 10,
+flexDirection: "column",
+textAlign: "center",
+alignItems: "center",
+height: "100%",
+}}
+initial={{ opacity: 0 }}
+animate={{ opacity: hovered ? 1 : 0 }}
+transition={{ duration: 0.5 }}
+>
+
+<Typography variant="body1" align="center" color="white">
+{description}
+</Typography>
+
+<Box
+borderRadius={"100%"}
+bgcolor={"#61a05f"}
+padding={"8px 8px 6px 10px"}
+color={"#0b260b"}
+onClick={handleCardClick}
+sx={{
+cursor: "pointer",
+zIndex: 4,
+}}
+>
+
+<ArrowForwardIosIcon />
+
+</Box>
+
+</motion.div>
+
+</Card>
+
+</motion.div>
+
+</motion.div>
+
+</Grid>
+);
+};
 
 const Services = () => {
-  return (
-    <Box
-      sx={{ backgroundColor: "#305041",
-        padding: { xs: "35px 10px 45px 10px", md: "50px", lg:"50px 10px 50px 10px" },
-       }}
-    >
-      <Typography variant="H38px" align="center" color="textThird">
-        Featured Projects
-      </Typography>
-      <Grid
-        mt={1}
-        justifySelf={"center"}
-        mb={6}
-        sx={{
-          backgroundColor: "#61a05f",
-          width: "120px",
-          height: "2px",
-          position: "relative",
-          overflow: "hidden",
-          "::before": {
-            content: '""',
-            position: "absolute",
-            top: 0,
-            left: "-120px",
-            width: "120px",
-            height: "2px",
-            background: "linear-gradient(to right, transparent, #fff, transparent)",
-            animation: "moveLight 2.5s infinite",
-          },
-        }}
-      >
-        <style>
-          {`
-            @keyframes moveLight {
-              0% {
-                left: -120px;
-              }
-              100% {
-                left: 120px;
-              }
-            }
-          `}
-        </style>
-      </Grid>
-      <Grid container gap={3} justifyContent="center">
-        {servicesData.map((service, index) => (
-          <ServiceCard
-            key={index}
-            index={index}
-            title={service.title}
-            description={service.description}
-            icon={service.icon}
-            link={service.link}
-          />
-        ))}
-      </Grid>
-    </Box>
-  );
+
+const [activeIndex, setActiveIndex] = useState(0);
+
+useEffect(() => {
+
+const interval = setInterval(() => {
+
+setActiveIndex((prev) => (prev + 1) % servicesData.length);
+
+}, 1200);
+
+return () => clearInterval(interval);
+
+}, []);
+
+return (
+
+<Box
+sx={{
+backgroundColor: "#305041",
+padding: { xs: "35px 10px 45px 10px", md: "50px", lg: "50px 10px 50px 10px" },
+}}
+>
+
+<Typography variant="H38px" align="center" color="textThird">
+Featured Projects
+</Typography>
+
+<Grid
+mt={1}
+justifySelf={"center"}
+mb={6}
+sx={{
+backgroundColor: "#61a05f",
+width: "120px",
+height: "2px",
+position: "relative",
+overflow: "hidden",
+"::before": {
+content: '""',
+position: "absolute",
+top: 0,
+left: "-120px",
+width: "120px",
+height: "2px",
+background: "linear-gradient(to right, transparent, #fff, transparent)",
+animation: "moveLight 2.5s infinite",
+},
+}}
+>
+
+<style>
+{`
+@keyframes moveLight {
+0% { left: -120px; }
+100% { left: 120px; }
+}
+`}
+</style>
+
+</Grid>
+
+<Grid container gap={3} justifyContent="center">
+
+{servicesData.map((service, index) => (
+
+<ServiceCard
+key={index}
+index={index}
+activeIndex={activeIndex}
+title={service.title}
+description={service.description}
+icon={service.icon}
+link={service.link}
+/>
+
+))}
+
+</Grid>
+
+</Box>
+
+);
 };
 
 export default Services;
